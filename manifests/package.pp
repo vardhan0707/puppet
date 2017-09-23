9d2b6647b6535a252d190a800131e54912915b3b
Sivalal <sivalalbotu@users.noreply.github.com>
2017-09-23 03:45:06 -0400

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,13 +1,3 @@
-package { 'ruby' :
-ensure =>installed,
-provider => brew,
-}
-
-package { 'bundler':
-ensure => installed,
-provider => gem,
-}
-
 package { 'puppet-lint' :
 ensure => installed,
 provider => puppet_gem,
