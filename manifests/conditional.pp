c6ebb5c7f5e9e0ba26002f62aa380bb0680956bc
sivalal.botu <sivalalbotu@gmail.com>
2017-09-23 23:56:32 -0400

    fix conditional.pp

--- manifests/conditional.pp
+++ manifests/conditional.pp
@@ -3,7 +3,7 @@ $install_curl =true
 if $install_curl {
 package {'curl' :
 ensure=> present,
-provider => brew,
+provider => 'brew',
 }
 notice('Installed curl as insall_curl value is ${install_curl}')
 } else {
