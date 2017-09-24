3f81a4b4780f9f1fbc9ea992755abba7ff32d92a
sivalal.botu <sivalalbotu@gmail.com>
2017-09-23 23:13:10 -0400

    fix exec_user manifest

--- manifests/exec_user.pp
+++ manifests/exec_user.pp
@@ -1,5 +1,5 @@
 exec { 'say-hello' :
-command => '/bin/echo Hello, this is 'whoami' > /tmp/hello-lal.txt',
+command => '/bin/echo Hello, this is && whoami > /tmp/hello-lal.txt',
 user => 'lal',
 creates => '/tmp/hello-lal.txt',
 }
