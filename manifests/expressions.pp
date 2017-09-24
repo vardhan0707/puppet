eaad9ffa39693e799d88574611337b7632c4d0dc
sivalal.botu <sivalalbotu@gmail.com>
2017-09-23 23:40:35 -0400

    testing expressions

---
+++ manifests/expressions.pp
@@ -0,0 +1,17 @@
+$value = (17 * 8) + (12/4) - 1
+notice($value)
+
+notice(9<10)
+notice(10>11)
+notice(10 >= 10)
+notice('foo' == 'foo')
+
+$test ='test'
+notice('test' == $test)
+notice('foo' in 'fool')
+notice('foo' in ['fool','foo','foobar'])
+notice('foo' in {'foo' => 'bar'})
+notice('foo' =~ /oo/)
+notice('foo' =~ String)
+notice( 1 !=2)
+
