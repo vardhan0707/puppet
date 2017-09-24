972392966533430e1491741aba4f486f3fd7bf18
sivalal.botu <sivalalbotu@gmail.com>
2017-09-23 23:50:00 -0400

    install curl based on conditional statement

---
+++ manifests/conditional.pp
@@ -0,0 +1,13 @@
+$install_curl =true,
+if $install_curl {
+package {'curl' :
+ensure=> present,
+provider => brew,
+}
+notice('Installed curl as insall_curl value is' $install_curl)
+} else {
+package {'curl' :
+esnure => absent,
+}
+}
+
